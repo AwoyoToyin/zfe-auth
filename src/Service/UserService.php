84e82be7b792e3a854291afff0d3917c3240a0f0
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2018-04-25 00:36:47 +0100

    Removed unnecessary codes

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -11,20 +11,5 @@ use Zfe\Common\Service\AbstractService;
 
 class UserService extends AbstractService
 {
-    /**
-     * Serves the index of all entity
-     *
-     * @param array $filters
-     * @param array $orderBy
-     * @param array $groupBy
-     * @return EntityInterface
-     */
-    public function index(
-        array $filters = [],
-        $orderBy = [],
-        $groupBy = []
-    ) {
-        return parent::index($filters, $orderBy, $groupBy);
-        // return $this->provider->selectAllPaginate(0, 1, $filters, $orderBy, $groupBy, false);
-    }
+
 }
